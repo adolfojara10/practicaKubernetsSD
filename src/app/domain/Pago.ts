19b2c75cfcb4d33a987127ddf443c102ae92dde0
adolfojara10 <gavilanesadolfo@gmail.com>
2022-07-04 08:23:13 -0500

    Pagos

---
+++ src/app/domain/Pago.ts
@@ -0,0 +1,9 @@
+export class pagoS {
+    id: number=0;
+    monto: number= 0;
+    metodoPago: String= "";
+    fecha: Date= new Date();
+    idUsuario: number= 0;
+    idCurso: number= 0;
+    
+}
